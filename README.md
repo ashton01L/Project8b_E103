81b1b37f671d217fddcb7a7c73e30a0b45938f28
ashton01L <ashtonamazing@gmail.com>
2024-08-08 23:53:01 -0500

    final version

---
+++ README.md
@@ -0,0 +1,3 @@
+Define a function named 'words_in_both' that takes two strings
+as parameters and returns a Python set containing only those
+words that appear in both strings.
---
+++ words_in_both.py
@@ -0,0 +1,34 @@
+# Author: Ashton Lee
+# Github User: ashton01L
+# Date: 8/8/2024
+# Description: Define a function named 'words_in_both' that takes two strings
+# as parameters and returns a Python set containing only those
+# words that appear in both strings.
+def words_in_both(string1, string2):
+    """
+    Finds and returns a set of words that appear in both input strings, ignoring case.
+    The words in the returned set are in all lower-case.
+
+    :param string1: (str): The first input string.
+           string2: (str): The second input string.
+
+    :return: set: A set of words that are common to both string1 and string2, in lower-case.
+    """
+
+    # Split the strings into lists of words and convert them to lower-case sets
+    word_set1 = string1.lower().split()
+    word_set2 = string2.lower().split()
+
+    common_words = []
+
+    # Iterate over the words in the word_set1 with a for loop
+    for word in word_set1:
+        # Cross check those words against word_set2 and to ensure they are not already in common_words
+        if word in word_set2 and word not in common_words:
+            common_words.append(word)
+
+    return common_words
+
+# common_words = words_in_both("She is a jack of all trades", "Jack was tallest of all")
+# test output: ['jack', 'of', 'all']
+# print(common_words)
